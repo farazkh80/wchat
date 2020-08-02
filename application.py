f08c04b417ddd5e5b23c4cd123a5e015536f1198
farazkh8 <farazkhoubsirat80@gmail.com>
2020-08-02 12:10:20 -0700

    added the wtf-forms and validators

--- application.py
+++ application.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    """reg_form = RegistrationForm()"""
+    reg_form = RegistrationForm()
+
+    """trigger validators"""
+    if reg_form.validate_on_submit():
+        return "Success"
+
     """returns rendered html pages in that route"""
-    return render_template("index.html")
+    return render_template("index.html", form=reg_form)
 
 
 if __name__ == "__main__":
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
+
 class RegistrationForm(FlaskForm):
-    """ Registration Form
-    """
-    username = StringField('username_label')
-    password = PasswordField('password_field')
-    confirm_password = PasswordField('confirm_password_label')
+    """ Registration Form Parameters and Validators"""
+    username = StringField('username_label',
+                           validators=[InputRequired(message="Username Required"),
+                                       Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+
+    password = PasswordField('password_label',
+                             validators=[InputRequired(message="Password Required"),
+                                         Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+    confirm_password = PasswordField('confirm_password_label',
+                                     validators=[InputRequired(message="Password Required"),
+                                                 EqualTo('password', message="Passwords must match")])
+    submit_button = SubmitField('Submit')

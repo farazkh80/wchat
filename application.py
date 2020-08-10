983ce784cf9ef32e2fbea6627816fb032e0dcd31
farazkh8 <farazkhoubsirat80@gmail.com>
2020-08-09 22:29:39 -0700

    added the database configuration to the basic login form

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
 from wtform_fields import *
+from models import *
 
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
+#configure database
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://xwawslekazwoni:ba0ee8e746269e7cc8ff5804aa28c3205e0309757bb2673ffad22d793bb13b92@ec2-34-225-162-157.compute-1.amazonaws.com:5432/dcqg3bakntjhp'
+
+db = SQLAlchemy(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -12,7 +17,18 @@ def index():
 
     """trigger validators"""
     if reg_form.validate_on_submit():
-        return "Success"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        #checks for its uniqueness
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Someone else has taken this username!"
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "inserted in the database"
 
     """returns rendered html pages in that route"""
     return render_template("index.html", form=reg_form)
@@ -20,3 +36,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
